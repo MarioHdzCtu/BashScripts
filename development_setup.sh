1479428f9205b52d2f8a85486d4eaa614850e63d
Mario Ubuntu Python <mariohertu@gmail.com>
2024-01-28 18:40:23 -0600

    Added installation for python extensions

--- development_setup.sh
+++ development_setup.sh
@@ -2,18 +2,30 @@
 cd ~
 sudo apt update && sudo apt upgrade -y
 #Install Git
+echo "Installing git"
 sudo apt install git -y
 #Install ssh
+echo "Installing ssh"
 sudo apt install ssh -y
 #Install VS Code
+echo "Installing VS Code"
 sudo apt install software-properties-common-apt-transport-https wget -
 wget -q https://packages.microsoft.com/keys/microsoft.asc -O- | sudo apt-key add -
 sudo add-apt-repository "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main"
 sudo apt update
 sudo apt install code
-#VSCODE customization
+#VSCODE extensions installation
+echo "Installing VS Code extensions"
 sudo apt install jq
 code --install-extension enkia.tokyo-night
+code --install-extension ms-python.python
+code --install-extension ms-python.isort
+code --install-extension python.vscode-pylance
+code --install-extension njpwerner.autodocstring
+code --install-extension team.vscode-icons
+
+#VSCODE Theme setter
+echo "Setting VSCode Theme"
 config_file=~/.config/Code/User/settings.json
 keys=$(jq '. | keys' $config_file)
 if [ -z "$keys" ];
